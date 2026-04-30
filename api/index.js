9e92538a2a47c4237bb5edd94d1fcd95a3c13879
tomas <mp@Tomino.local>
2026-04-30 13:30:09 +0200

    fix

--- api/index.js
+++ api/index.js
@@ -1,37 +1,61 @@
-import express, { json, urlencoded } from "express"
-import pkg from "@prisma/client"; // 1. Importujeme celý balíček jako výchozí export
-const { PrismaClient } = pkg;
+import "dotenv/config";
+import express, { json, urlencoded } from "express";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
 
 const port = 3000;
-const app = express()
-
-const prisma = new PrismaClient();
-
-app.use(urlencoded(json))
-
-app.listen(port, ()=> {
-    
-    console.log("Server running on port: ", port )
-})
-
-app.get("/", (req, res)=>{
+const app = express();
 
 
-    return res.json("API ready!")
-})
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-app.post("/post", (req, res)=> {
-    return res.json("Post not implemented!")
-})
+app.listen(port, () => {
+    console.log("Server running on port: ", port);
+});
 
-app.put("/put", (req, res)=> {
-    return res.json("Put not implemented!")
-})
+app.get("/", (req, res) => {
+    return res.json("API ready!");
+});
 
-app.patch("/patch", (req, res)=> {
-    return(res.json("Patch not implemented!"))
-})
+app.post("/addUser", async (req, res) => {
+    try {
+    const {id, email, name, surname} = req.body 
+     
+    const newUser = await prisma.user.create({
+        data: {
+            id: Number(id),
+            email: email,
+            name: name,
+            surname: surname
+        }
+    })
+    return res.json({
+        message: "Added: ",
+        user:newUser
+    });
+    } catch(err){
+        return res.json({
+            message: "Database not working or user already exists!",
+            error: err
+        })
+    }
+});
+
+app.put("/put", (req, res) => {
+    return res.json("Put not implemented!");
+});
+
+app.patch("/patch", (req, res) => {
+    return res.json("Patch not implemented!");
+});
 
 app.delete("/delete", (req, res) => {
-    return res.json("Delete not implemented!")
-})
+    return res.json("Delete not implemented!");
+});
