4a8efad36811bab4f3993edc66e69ab1c1aebccd
tomas <ulricht@email.cz>
2026-05-12 16:15:38 +0200

    done categories in post

--- web/components/AddPost/Form.tsx
+++ web/components/AddPost/Form.tsx
@@ -1,8 +1,11 @@
 import { use, useState } from "react"
+import categoryOptions from "@/js/categoryOptions"
 
 export default function Form(){
     const [name, setName] = useState("") || null
     const [post, setPost] = useState("") || null
+    const [categories, setCategories] = useState(categoryOptions) 
+    const [categoriesActive, setCategoriesActive] = useState(false)
 
     const onChangeName = (e) => {
         setName(e.target.value)
@@ -11,12 +14,36 @@ export default function Form(){
     const onChangePost = (e) => {
         setPost(e.target.value)
     }
+
+    const toggleActive = () => {
+        setCategoriesActive(!categoriesActive)
+    }
     
     return(
         <div className="flex flex-col gap-2 border-gray-300 border-1 p-3 rounded-2xl">
             <div className="flex gap-10">
                 <input className="border-1 px-2 border-gray-300 w-1/3 rounded-xl" type="text" value={name} onChange={onChangeName} placeholder="Název příspěvku"/>
-                <button className="border-1 px-5 border-gray-300 rounded-xl">Kategorie</button>
+                {!categoriesActive && (
+                <button 
+                    className="border-1 px-5 border-gray-300 rounded-xl"
+                    onClick={toggleActive}
+                    >
+                        Kategorie
+                        </button>
+                )}
+                {
+                    categoriesActive && (
+                        <div className="flex gap-5">
+                        {categories.map((category, index)=> (
+                            <button 
+                                key={index}
+                                className="border-1 px-5 border-gray-300 rounded-xl"
+                                onClick={toggleActive}>{category}</button>
+                        ))}
+                        </div>
+                        
+                    )
+                }
             </div>
             <textarea className="border-1 h-30 px-2 text-wrap border-gray-300 rounded-xl" value={post} onChange={onChangePost} placeholder="Obsah příspěvku ..."/>
         </div>
